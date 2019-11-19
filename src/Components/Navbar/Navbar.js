fbd3791f4da84850a7678c8cafb7350ce58b86d5
oribuhbut <51500172+oribuhbut@users.noreply.github.com>
2019-11-20 01:56:39 +0200

    Update Navbar.js

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,45 +4,45 @@ import InputRange from 'react-input-range';
 import 'react-input-range/lib/css/index.css'
 class Navbar extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.generateArray = this.generateArray.bind(this);
+        this.setLength = this.setLength.bind(this);
+    }
+
     componentDidMount() {
-        this.state.arr = Array.from({ length: this.state.value }, () => Math.floor(Math.random() * 100 + 10))
-        for (let i = 0; i < this.state.arr.length; i++) {
-            this.state.arr[i] = Object.create(null);
-            this.state.arr[i].index = i;
-            this.state.arr[i].value = Math.floor(Math.random() * 100 + 10)
-            this.setState({})
-        }
-        this.props.arrChange(this.state.arr);
+        this.generateArray()
     }
 
     state = {
         value: 10,
-        sortType: "",
+        type: "",
         arr: [],
-        alert: "",
+        info: ""
     }
 
-    sendArr(val) {
-        this.state.arr = Array.from({ length: val })
+    generateArray() {
+        this.state.arr = Array.from({ length: this.state.value })
         for (let i = 0; i < this.state.arr.length; i++) {
             this.state.arr[i] = Object.create(null);
             this.state.arr[i].index = i;
             this.state.arr[i].value = Math.floor(Math.random() * 100 + 10)
             this.setState({})
         }
-        this.state.value = val;
-        console.log(this.state.arr)
-        this.setState({})
-        this.props.arrChange(this.state.arr);
+        this.props.arrOnChange(this.state.arr);
+    }
+
+    setLength(val) {
+        this.setState({ value: val }, () => this.generateArray());
     }
 
     sortFunction() {
-        if (!this.state.sortType.length) {
-            this.state.alert = "Select Sort Type"
+        if (!this.state.type.length) {
+            this.state.info = "Select Sort Type"
             this.setState({});
             return;
         }
-        this.props.getSort(this.state.sortType);
+        this.props.sort(this.state.type);
     }
 
     render() {
@@ -50,24 +50,23 @@ class Navbar extends React.Component {
             <div className="row-fluid">
                 <div className="nav-container">
                     <div className="trigger">
-                        <div onClick={this.sortFunction.bind(this)} className="lead">
+                        <div disabled={this.props.disabled} onClick={this.sortFunction.bind(this)} className="lead">
                             Generate New Array Sort
                         </div>
                     </div>
                     <div className="slider">
                         <InputRange
-                            disabled={this.props.inputRange}
+                            disabled={this.props.disabled}
                             maxValue={100}
                             minValue={1}
                             value={this.state.value}
-                            onChange={this.sendArr.bind(this)} />
+                            onChange={this.setLength.bind(this)} />
                     </div>
-                    <div className="alert lead">{this.state.alert}</div>
+                    <div className="info lead">{this.state.info}</div>
                     <div className="nav-ul">
-                        <li><span onClick={() => this.setState({ sortType: "Merge", alert: "Merge Sort" })}>Merge Sort</span></li>
-                        <li><span onClick={() => this.setState({ sortType: "Quick", alert: "Quick Sort" })}>Quick Sort</span></li>
-                        <li><span onClick={() => this.setState({ sortType: "Heap", alert: "Heap Sort" })}>Heap Sort</span></li>
-                        <li><span onClick={() => this.setState({ sortType: "Bubble", alert: "Bubble Sort" })}>Bubble Sort</span></li>
+                        <li><span disabled={this.props.disabled} onClick={() => this.setState({ type: "Quick", info: "Quick Sort" })}>Quick Sort</span></li>
+                        <li><span disabled={this.props.disabled} onClick={() => this.setState({ type: "Heap", info: "Heap Sort" })}>Heap Sort</span></li>
+                        <li><span disabled={this.props.disabled} onClick={() => this.setState({ type: "Bubble", info: "Bubble Sort" })}>Bubble Sort</span></li>
                     </div>
                 </div>
             </div>
